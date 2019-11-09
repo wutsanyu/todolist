a512962b0342e8bebd58a7325477ba53eb68fcf5
wutsanyu <b97101053@gmail.com>
2019-11-09 21:00:39 +0800

    change tasks column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_041341) do
+ActiveRecord::Schema.define(version: 2019_11_09_125906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,13 +33,13 @@ ActiveRecord::Schema.define(version: 2019_11_09_041341) do
   create_table "tasks", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
-    t.integer "sequence"
-    t.integer "status"
     t.text "description"
     t.string "title"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "sequence"
+    t.integer "status"
   end
 
   create_table "users", force: :cascade do |t|
