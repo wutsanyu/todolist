921c9079b6a23b223ca581d11715db2446eec577
wutsanyu <b97101053@gmail.com>
2019-11-11 20:31:33 +0800

    add before_action: find_task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  
+  before_action :find_task, only: [:edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -18,30 +18,29 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find_by(id: params[:id])
+    
   end
 
   def update
-    @task = Task.find_by(id: params[:id])
     if @task.update(task_params)
-      redirect_to tasks_path, notice: "任務更新成功"
+      redirect_to tasks_path, notice: "任務更新成功!"
     else
       render :edit
     end
   end
 
-  def show
-    
-  end
 
   def destroy
-    @task = Task.find_by(id: params[:id])
     @task.destroy if @task
-    redirect_to tasks_path, notice: "任務已刪除"
+    redirect_to tasks_path, notice: "任務已刪除!"
   end
 
   private
   def task_params
     params.require(:task).permit(:title, :sequence, :tag_list, :status, :start_time, :end_time, :description)
+  end
+
+  def find_task
+    @task = Task.find_by(id: params[:id])
   end
 end
